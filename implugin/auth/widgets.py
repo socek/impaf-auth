eaa839fd9b527f34958427f2ce1b57cb04322e1d
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2015-12-01 14:09:03 +0100

    New version of template injection.

--- implugin/auth/widgets.py
+++ implugin/auth/widgets.py
@@ -3,12 +3,6 @@ from implugin.formskit.widget import FormWidget
 
 class LoginFormWidget(FormWidget):
 
-    _login_form_templates = {
-        'password': 'implugin.auth.widgets:/templates/forms/password.jinja2',
-        'text': 'implugin.auth.widgets:/templates/forms/text.jinja2',
-    }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.templates = dict(super().templates)
-        self.templates.update(self._login_form_templates)
+    class Templates(FormWidget.Templates):
+        password = 'implugin.auth.widgets:/templates/forms/password.jinja2'
+        text = 'implugin.auth.widgets:/templates/forms/text.jinja2'
